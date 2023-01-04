d76987c5d181a655568e6e461ff1b4eb64bed875
dedenbangkit <mail@dedenbangkit.com>
2023-01-05 06:50:19 +0700

    [#1] Update tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,11 @@
 import unittest
 
-from AkvoResponseGrouper.example import Example
+from AkvoResponseGrouper import response_grouper
 
 
 class TestExample(unittest.TestCase):
     def test_add(self):
-        self.assertEqual((Example(5) + Example(6)).value, 11)
+        self.assertEqual((response_grouper(5) + response_grouper(6)).value, 11)
 
 
 if __name__ == '__main__':
