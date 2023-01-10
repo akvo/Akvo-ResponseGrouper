08533526756e58fc31f98cc600c7e27963c5246a
dedenbangkit <mail@dedenbangkit.com>
2023-01-10 17:35:10 +0700

    [#2] Update version

--- dev/backend/start.sh
+++ dev/backend/start.sh
@@ -7,6 +7,10 @@ pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
 pip check
 
+if [[ -z "${INSTALL}" ]]; then
+	pip install response_grouper==1.0.5
+fi
+
 alembic upgrade head
 
 uvicorn main:app --reload --port 5000 --host 0.0.0.0
--- src/AkvoResponseGrouper/__init__.py
+++ src/AkvoResponseGrouper/__init__.py
@@ -1,4 +1,4 @@
 from .response_grouper import response_grouper
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ["response_grouper"]
