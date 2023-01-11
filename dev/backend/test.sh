57bd1a2e4964904554334ea18e3b76894a8630a6
dedenbangkit <mail@dedenbangkit.com>
2023-01-11 12:19:30 +0700

    [#2] Fix CI test

--- dev/backend/test.sh
+++ dev/backend/test.sh
@@ -2,6 +2,14 @@
 
 set -eo pipefail
 
+find ./AkvoResponseGrouper -maxdepth 0 -empty -exec echo {} is empty. \;
+
+pip -q install --upgrade pip
+pip -q install --cache-dir=.pip -r requirements.txt
+pip check
+
+alembic upgrade head
+
 python -m AkvoResponseGrouper.cli.migrate -c ./sources/category.json
 
 pytest -vvv -rP
