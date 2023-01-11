080c0c77bef0267e35d00e23a0c9e135068a2ded
ifirmawan <firmawaneiwan@gmail.com>
2023-01-11 11:46:11 +0700

    [#5] Create filtering for category endpoint

--- src/AkvoResponseGrouper/db/crud_category.py
+++ src/AkvoResponseGrouper/db/crud_category.py
@@ -1,7 +1,25 @@
-from typing import List
+from typing import List, Optional
 from sqlalchemy.orm import Session
+from sqlalchemy import func
+from sqlalchemy.sql.operators import ilike_op
 from ..models.Category import Category, CategoryDict
 
 
-def get_categories(session: Session) -> List[CategoryDict]:
-    return session.query(Category).all()
+def get_categories(
+    session: Session,
+    id: Optional[int] = None,
+    data: Optional[int] = None,
+    name: Optional[int] = None,
+    category: Optional[int] = None,
+) -> List[CategoryDict]:
+    queries = []
+    if id:
+        queries.append(Category.id == id)
+    if data:
+        queries.append(Category.data == data)
+    if name:
+        queries.append(func.lower(Category.name) == func.lower(name))
+    if category:
+        queries.append(ilike_op(Category.category, f'%{category}%'))
+
+    return session.query(Category).filter(*queries).all()
