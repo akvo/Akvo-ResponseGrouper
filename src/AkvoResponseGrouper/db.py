d3ce5bcb3815acda37f9a132bf27b2f254ab22a8
ifirmawan <firmawaneiwan@gmail.com>
2023-01-12 10:24:26 +0700

    [#5] Fix declarative_base not found in library

--- src/AkvoResponseGrouper/db.py
+++ src/AkvoResponseGrouper/db.py
@@ -1,6 +1,7 @@
 from os import environ
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 def get_db_url():
