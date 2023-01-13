9df61906c0681e1f9cc0971ee6532540e2fcbe84
anjarakvo <anjar@akvo.org>
2023-01-13 15:48:17 +0700

    [#10] remove debugging commands

--- dev/backend/test.sh
+++ dev/backend/test.sh
@@ -2,16 +2,11 @@
 
 set -euo pipefail
 
-cd /app/dev/backend
-
 find ./AkvoResponseGrouper -maxdepth 0 -empty -exec echo {} is empty. \;
 
 pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
 pip check
-
-pwd
-ls -la
 
 alembic upgrade head
 
@@ -22,19 +17,10 @@ COVERAGE_PROCESS_START=./.coveragerc \
   coverage run --parallel-mode --concurrency=thread,gevent --rcfile=./.coveragerc \
   /usr/local/bin/pytest -vvv -rP
 
-
-
-echo "===Coverage==="
+echo "===Combining==="
 coverage combine --rcfile=./.coveragerc
-pwd
-ls -la
-
-
 echo "===Reporting==="
 coverage report -m --rcfile=./.coveragerc
-pwd
-ls -la
-
 
 if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
   coveralls
