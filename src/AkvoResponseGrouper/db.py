5f2e830960e5b33f306cf5f98c3449a8207aaade
ifirmawan <firmawaneiwan@gmail.com>
2023-01-18 16:30:56 +0700

    [#5] Replace hyphen in DATABASE_URL
    We still need to replace the dash as it is still a database connection
    problem when requests from endpoints are executed.

--- src/AkvoResponseGrouper/db.py
+++ src/AkvoResponseGrouper/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 def get_db_url():
     TESTING = environ.get("TESTING")
     DATABASE_URL = environ["DATABASE_URL"]
+    DATABASE_URL = DATABASE_URL.replace("-", "_")
     DB_URL = f"{DATABASE_URL}_test" if TESTING else DATABASE_URL
     return DB_URL
 
