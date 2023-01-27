fd89e0da13b80511a8abeae8a4703db2336d0498
ifirmawan <firmawaneiwan@gmail.com>
2023-01-27 19:19:54 +0700

    [#5] Fix test filtering by data

--- dev/backend/tests/test_03_views.py
+++ dev/backend/tests/test_03_views.py
@@ -1,12 +1,20 @@
 import sys
 import pytest
+import random
 from fastapi import FastAPI
 from httpx import AsyncClient
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, load_only
 from AkvoResponseGrouper.views import get_categories
+from AkvoResponseGrouper.models import Category
 
 pytestmark = pytest.mark.asyncio
 sys.path.append("..")
+
+
+def get_random_data(session: Session):
+    return random.choice(
+        session.query(Category).options(load_only("data")).all()
+    )
 
 
 class TestViews:
@@ -38,5 +46,6 @@ class TestViews:
     async def test_views_filtering_by_data(
         self, app: FastAPI, session: Session, client: AsyncClient
     ) -> None:
-        res = get_categories(session=session, data=[1])
+        random = get_random_data(session=session)
+        res = get_categories(session=session, data=random.data)
         assert len(res) > 0
