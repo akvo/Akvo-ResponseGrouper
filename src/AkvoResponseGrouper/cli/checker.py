2f7095d4205fdef7f175bd55ff23cdad9b8205cd
ifirmawan <firmawaneiwan@gmail.com>
2023-02-02 13:58:07 +0700

    [#16] Fix assert & fake_data in category checker
    - Add assertion more specific in test_category_checker.py
    - Add data type for all param in each method checker.py

--- src/AkvoResponseGrouper/cli/checker.py
+++ src/AkvoResponseGrouper/cli/checker.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from collections import defaultdict, Counter
 from itertools import groupby
 from operator import itemgetter
@@ -6,7 +7,12 @@ from ..utils import get_intersection
 
 
 def append_duplicates(
-    category_check, category, config, duplicates, types, ck
+    category_check: dict,
+    category: dict,
+    config: dict,
+    duplicates: dict,
+    types: str,
+    ck: dict,
 ) -> None:
     if category_check["name"] != category["name"]:
         if ck["question"] == config["question"]:
@@ -23,7 +29,7 @@ def append_duplicates(
 
 
 def loop_duplicates(
-    category, config, category_check, duplicates: list
+    category: dict, config: dict, category_check: dict, duplicates: list
 ) -> None:
     for types in ["and", "or"]:
         if types in category_check:
@@ -38,7 +44,7 @@ def loop_duplicates(
                 )
 
 
-def get_duplicates(category, categories):
+def get_duplicates(category: dict, categories: list):
     total = 1 if "or" in category else 0
     configs = []
     if "or" in category:
@@ -61,7 +67,9 @@ def get_duplicates(category, categories):
     return configs, duplicates, total
 
 
-def append_duplicates_dict(duplicate, duplicates, duplicates_dict):
+def append_duplicates_dict(
+    duplicate: dict, duplicates: list, duplicates_dict: dict
+):
     for types in ["and", "or"]:
         dp = len(
             list(
@@ -88,7 +96,7 @@ def append_duplicates_dict(duplicate, duplicates, duplicates_dict):
                 }
 
 
-def merge_grouped_data(data) -> list:
+def merge_grouped_data(data: list) -> list:
     merged_data = []
     for key, values in data.items():
         merged = {"name": key}
@@ -98,7 +106,7 @@ def merge_grouped_data(data) -> list:
     return merged_data
 
 
-def get_uniq_categories(categories) -> list:
+def get_uniq_categories(categories: list) -> list:
     names = [o["name"] for o in categories]
     dn = [item for item, count in Counter(names).items() if count > 1]
     ld = list(filter(lambda d: d["name"] in dn, categories))
@@ -113,7 +121,9 @@ def get_uniq_categories(categories) -> list:
     return categories
 
 
-def get_options(data) -> list:
+def get_options(data: dict) -> list:
+    if "categories" not in data:
+        sys.exit(0)
     options = []
     categories = [c for c in data["categories"]]
     categories = get_uniq_categories(categories=categories)
@@ -140,7 +150,9 @@ def get_options(data) -> list:
     return options
 
 
-def loop_options(opt: dict, td: dict, printed: dict, title: str) -> None:
+def loop_options(
+    opt: dict, td: dict, printed: dict, title: str = None, info: bool = True
+) -> None:
     if opt["total_duplicate"][td]["total"] >= opt["total"]:
         if opt["total_duplicate"][td]["or"] == len(opt["or"]) and opt[
             "total_duplicate"
@@ -148,20 +160,24 @@ def loop_options(opt: dict, td: dict, printed: dict, title: str) -> None:
             duplicate = list(
                 filter(lambda x: x["name"] == td, opt["duplicates"])
             )
-            print(
-                f"{title}\n", f"POTENTIAL DUPLICATE: {opt['name']} WITH {td}"
-            )
-            for d in duplicate:
+            if info:
                 print(
-                    f"""
-                    QUESTION: {d['question']}
-                    OPTIONS: {d['option']}
-                    """
+                    title,
+                    f"POTENTIAL DUPLICATE: {opt['name']} WITH {td}",
                 )
+            for d in duplicate:
+                if info:
+                    print(
+                        f"""
+                        QUESTION: {d['question']}
+                        OPTIONS: {d['option']}
+                        """,
+                        "===================================",
+                    )
                 printed.update({td: True})
 
 
-def check_config(file_config) -> bool:
+def check_config(file_config: str, info: bool = True) -> int:
     with open(file_config) as f:
         data = f.read()
         data = json.loads(data)
@@ -173,9 +189,10 @@ def check_config(file_config) -> bool:
             if opt["name"] in printed:
                 continue
             for td in opt["total_duplicate"]:
-                loop_options(opt=opt, td=td, printed=printed, title=d["name"])
+                title = d["name"] if "name" in d else None
+                loop_options(
+                    opt=opt, td=td, printed=printed, title=title, info=info
+                )
         if len(printed) > 0:
-            counter += 1
-            print("===================================================\n")
-
-    return True if counter > 0 else False
+            counter += len(printed)
+    return counter
--- tests/test_category_checker.py
+++ tests/test_category_checker.py
@@ -1,187 +1,137 @@
 import unittest
-from AkvoResponseGrouper.cli.checker import check_config
+from AkvoResponseGrouper.cli.checker import check_config, get_options
 from AkvoResponseGrouper.utils import generate_data_as_json_file
 
 
 class TestCategoryChecker(unittest.TestCase):
-    def test_if_duplicate_is_available(self):
-        fake_data = [
+    def test_if_category_is_typo(self):
+        with self.assertRaises(SystemExit) as cm:
+            data = {"name": "Water", "categoryies": []}
+            get_options(data=data)
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_name_is_not_present(self):
+        data = [
+            {
+                "form": 2,
+                "categories": [],
+            }
+        ]
+        fc = generate_data_as_json_file(data=data)
+        checker = check_config(file_config=fc, info=False)
+        self.assertFalse(checker)
+
+    def test_form_is_not_present(self):
+        data = [
             {
                 "name": "Water",
-                "form": "556240162",
+                "categories": [],
+            }
+        ]
+        fc = generate_data_as_json_file(data=data)
+        checker = check_config(file_config=fc, info=False)
+        self.assertFalse(checker)
+
+    def test_5C_duplicate_with_5CD(self):
+        data = [
+            {
+                "name": "Water",
+                "form": 1,
                 "categories": [
                     {
-                        "name": "Unimproved D",
+                        "name": "Category-1A",
                         "and": [
-                            {
-                                "question": 573340127,
-                                "options": ["Bottled water"],
-                            },
-                            {
-                                "question": 573340129,
-                                "options": ["Bottled water"],
-                            },
+                            {"question": 1, "options": ["A"]},
+                        ],
+                    },
+                ],
+            },
+            {
+                "name": "Sanitation",
+                "form": 1,
+                "categories": [
+                    {
+                        "name": "Category-5C",
+                        "and": [
+                            {"question": 5, "options": ["C"]},
                         ],
                     },
                     {
-                        "name": "Unimproved C",
-                        "or": [
-                            {
-                                "question": 573340127,
-                                "options": ["Bottled water"],
-                            },
-                            {"question": 999, "options": ["Bottled water"]},
-                        ],
+                        "name": "Category-5CD",
                         "and": [
-                            {
-                                "question": 573340129,
-                                "options": ["Bottled water"],
-                            }
+                            {"question": 5, "options": ["C", "D"]},
+                        ],
+                    },
+                ],
+            },
+        ]
+        fc = generate_data_as_json_file(data=data)
+        checker = check_config(file_config=fc, info=False)
+        self.assertEqual(checker, 1)
+
+    def test_1A_duplicate_with_1AB_and_1A_with_1AB2CD(self):
+        data = [
+            {
+                "name": "Water",
+                "form": 1,
+                "categories": [
+                    {
+                        "name": "Category-1A",
+                        "and": [
+                            {"question": 1, "options": ["A"]},
                         ],
                     },
                     {
-                        "name": "Surface Water",
-                        "and": [
-                            {
-                                "question": 573340127,
-                                "options": ["Surface water"],
-                            }
-                        ],
+                        "name": "Category-1AB",
+                        "and": [{"question": 1, "options": ["A", "B"]}],
                     },
                     {
-                        "name": "Unimproved",
+                        "name": "Category-1AB2CD",
                         "and": [
-                            {
-                                "question": 573340127,
-                                "options": [
-                                    "Bottled water",
-                                    "Unprotected spring",
-                                    "Unprotected dug well",
-                                ],
-                            },
-                            {
-                                "question": 573340129,
-                                "options": ["Bottled water"],
-                            },
-                        ],
-                    },
-                    {
-                        "name": "Limited",
-                        "and": [
-                            {
-                                "question": 573340127,
-                                "options": [
-                                    "Protected dug well",
-                                    "Public tap/standpipe",
-                                    (
-                                        "Piped water into dwelling (household"
-                                        " connection)"
-                                    ),
-                                    "Piped to neighbour",
-                                    "Piped water to yard/plot",
-                                    (
-                                        "Shared Deep tube well / shallow tube"
-                                        " well"
-                                    ),
-                                    "Shallow tubewell/borehole",
-                                    "Protected spring",
-                                    "Deep tubewell/borehole",
-                                ],
-                            },
-                            {
-                                "question": 573340128,
-                                "options": ["More than 30 minutes"],
-                            },
-                        ],
-                    },
-                    {
-                        "name": "Basic",
-                        "and": [
-                            {
-                                "question": 573340127,
-                                "options": [
-                                    "Protected dug well",
-                                    "Public tap/standpipe",
-                                    (
-                                        "Piped water into dwelling (household"
-                                        " connection)"
-                                    ),
-                                    "Piped to neighbour",
-                                    "Piped water to yard/plot",
-                                    (
-                                        "Shared Deep tube well / shallow tube"
-                                        " well"
-                                    ),
-                                    "Shallow tubewell/borehole",
-                                    "Protected spring",
-                                    "Deep tubewell/borehole",
-                                ],
-                            },
-                            {
-                                "question": 573340128,
-                                "options": [
-                                    "Less than 30 minutes",
-                                    "Don''t know",
-                                ],
-                            },
-                            {
-                                "question": 573340129,
-                                "options": [
-                                    "Yes, at least once",
-                                    "Don''t know",
-                                ],
-                            },
-                        ],
-                    },
-                    {
-                        "name": "Safely Managed",
-                        "and": [
-                            {
-                                "question": 573340127,
-                                "options": [
-                                    "Protected dug well",
-                                    "Public tap/standpipe",
-                                    (
-                                        "Piped water into dwelling (household"
-                                        " connection)"
-                                    ),
-                                    "Piped to neighbour",
-                                    "Piped water to yard/plot",
-                                    (
-                                        "Shared Deep tube well / shallow tube"
-                                        " well"
-                                    ),
-                                    "Shallow tubewell/borehole",
-                                    "Protected spring",
-                                    "Deep tubewell/borehole",
-                                ],
-                            },
-                            {
-                                "question": 573340128,
-                                "options": [
-                                    "Less than 30 minutes",
-                                    "Don''t know",
-                                ],
-                            },
-                            {
-                                "question": 573340129,
-                                "options": ["No, always sufficient"],
-                            },
-                            {
-                                "question": 573340125,
-                                "options": [
-                                    "Free from feacal and prioirty chemical"
-                                    " contamination"
-                                ],
-                            },
+                            {"question": 1, "options": ["A", "B"]},
+                            {"question": 2, "options": ["C", "D"]},
                         ],
                     },
                 ],
             }
         ]
-        file = generate_data_as_json_file(data=fake_data)
-        checker = check_config(file_config=file)
-        self.assertTrue(checker)
+        fc = generate_data_as_json_file(data=data)
+        checker = check_config(file_config=fc, info=False)
+        self.assertEqual(checker, 2)
+
+    def test_checker_config_is_passed(self):
+        data = [
+            {
+                "name": "Water",
+                "form": 1,
+                "categories": [
+                    {
+                        "name": "Category-1A3FG",
+                        "and": [
+                            {"question": 1, "options": ["A"]},
+                            {"question": 3, "options": ["F", "G"]},
+                        ],
+                    },
+                    {
+                        "name": "Category-1AB",
+                        "and": [
+                            {"question": 1, "options": ["C"]},
+                            {"question": 3, "options": ["A"]},
+                        ],
+                    },
+                    {
+                        "name": "Category-1AB2CD",
+                        "and": [
+                            {"question": 1, "options": ["A", "B"]},
+                            {"question": 2, "options": ["C", "D"]},
+                        ],
+                    },
+                ],
+            }
+        ]
+        fc = generate_data_as_json_file(data=data)
+        checker = check_config(file_config=fc, info=False)
+        self.assertFalse(checker)
 
 
 if __name__ == "__main__":
