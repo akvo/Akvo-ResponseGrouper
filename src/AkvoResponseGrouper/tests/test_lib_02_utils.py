32457e510355500bf6ae959d483283e4582f8d2e
dedenbangkit <mail@dedenbangkit.com>
2023-02-17 08:03:33 +0700

    [#22] Fix tests

--- src/AkvoResponseGrouper/tests/test_lib_02_utils.py
+++ src/AkvoResponseGrouper/tests/test_lib_02_utils.py
@@ -14,12 +14,13 @@ A test case for utils with pytest
 
 def test_transform_array():
     fake_array = [
-        {"category": "Basic", "name": "Category 1", "count": 10},
-        {"category": "Limited", "name": "Category 1", "count": 15},
+        {"category": "Basic", "name": "Category 1", "count": 10, "form": 1},
+        {"category": "Limited", "name": "Category 1", "count": 15, "form": 1},
     ]
     assert group_by_category_output(fake_array) == [
         {
             "category": "Category 1",
+            "form": 1,
             "options": [
                 {
                     "name": "Basic",
