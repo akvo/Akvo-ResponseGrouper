240986d7e1d230ff7a5797918eda5f9134b66adc
ifirmawan <firmawaneiwan@gmail.com>
2023-05-09 11:32:00 +0700

    [#31] Create unit test: validate_question_options

--- dev/backend/tests/test_02_category.py
+++ dev/backend/tests/test_02_category.py
@@ -5,6 +5,10 @@ from fastapi import FastAPI
 from httpx import AsyncClient
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+
+from ..core.db import get_db_url
+
 from AkvoResponseGrouper.cli.generate_schema import generate_schema
 from AkvoResponseGrouper.views import (
     get_categories,
@@ -12,6 +16,7 @@ from AkvoResponseGrouper.views import (
     refresh_view,
 )
 from scripts.seeder_datapoint import seed
+from AkvoResponseGrouper.db import validate_question_options
 
 pytestmark = pytest.mark.asyncio
 sys.path.append("..")
@@ -35,3 +40,28 @@ class TestMigration:
         refresh_view(session=session)
         res = get_categories(session=session)
         assert len(res) > 1
+
+    @pytest.mark.asyncio
+    async def test_if_question_n_options_exists(
+        self, app: FastAPI, session: Session, client: AsyncClient
+    ) -> None:
+        engine = create_engine(get_db_url())
+        with engine.connect() as connection:
+            ce = validate_question_options(
+                connection=connection, question=1, form=1, options=["Yes"]
+            )
+            assert ce == []
+            cy = validate_question_options(
+                connection=connection,
+                question=567800083,
+                form=554360198,
+                options=["Yes"],
+            )
+            assert cy == ["Yes"]
+            cn = validate_question_options(
+                connection=connection,
+                question=567800080,
+                form=554360198,
+                options=None,
+            )
+            assert cn == [1]
