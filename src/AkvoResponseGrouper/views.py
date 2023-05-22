edae17aa3ad5561aa3a10fc9a4be7e4ef2294370
ifirmawan <firmawaneiwan@gmail.com>
2023-05-22 19:54:21 +0700

    [#33]Filter get_categories from db except param category

--- src/AkvoResponseGrouper/views.py
+++ src/AkvoResponseGrouper/views.py
@@ -1,11 +1,24 @@
 from typing import List, Optional
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from .models import Category, CategoryDict
 from .utils import transform_categories_to_df, get_counted_category
 
 
-def get_data_categories(session: Session):
-    categories = session.query(Category).all()
+def get_data_categories(
+    session: Session,
+    form: Optional[int] = None,
+    name: Optional[str] = None,
+    data: Optional[List[int]] = None,
+):
+    queries = []
+    if form:
+        queries.append(Category.form == form)
+    if name:
+        queries.append(func.lower(Category.name) == name.lower())
+    if data:
+        queries.append(Category.data == data)
+    categories = session.query(Category).filter(*queries).all()
     categories = [c.serialize for c in categories]
     return categories
 
@@ -20,23 +33,15 @@ def get_categories(
     form: Optional[int] = None,
     name: Optional[str] = None,
     category: Optional[str] = None,
-    data: Optional[str] = None,
+    data: Optional[List[int]] = None,
 ) -> List[CategoryDict]:
-    res = get_results(session=session)
-    queries = []
-    if form:
-        queries.append(f"form == {form}")
-    if name:
-        queries.append(f"name.str.lower() == '{name.lower()}'")
+    categories = get_data_categories(
+        session=session, form=form, name=name, data=data
+    )
+    df = transform_categories_to_df(categories=categories)
     if category:
-        queries.append(f"category.str.lower() == '{category.lower()}'")
-    if data:
-        data = [int(d) for d in data.split(",")]
-        queries.append("data.isin(@data).values")
-    if len(queries):
-        queries = " & ".join(queries)
-        res = res.query(queries)
-    return res.to_dict("records")
+        df = df.query(f"category.str.lower() == '{category.lower()}'")
+    return df.to_dict("records")
 
 
 def get_group_by_category(
