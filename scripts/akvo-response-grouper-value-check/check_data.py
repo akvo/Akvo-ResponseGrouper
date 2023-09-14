41390acf70da2947fcbd1caee1e3e0853778be7e
wayangalihpratama <wgprtm@gmail.com>
2023-09-14 17:38:17 +0800

    [#37] Update check_data.py script

--- scripts/akvo-response-grouper-value-check/check_data.py
+++ scripts/akvo-response-grouper-value-check/check_data.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 import pandas as pd
 from AkvoResponseGrouper.utils import (
     get_category,
@@ -9,7 +10,17 @@ with open("./check/category.json", "r") as category_json:
     categories = json.loads(category_json.read())
 
 
-data = pd.read_csv("./check/siwins-2018-data-without-other.csv")
+data = pd.read_csv("./check/2018_data_export_new.csv")
+
+# Remove columns containing '--other--' or '--OTHER--'
+columns_to_remove = [col for col in data.columns if '--other--' in col.lower()]
+data.drop(columns=columns_to_remove, inplace=True)
+# Create an auto-incremented ID column
+data['id'] = np.arange(len(data)) + 1
+
+# save data without other
+pd.DataFrame(data).to_csv("./check/siwins-2018-data-without-other-new.csv", index=None)
+
 col_names = {d: d.split("|")[0] for d in list(data)}
 data = data.rename(columns=col_names)
 
